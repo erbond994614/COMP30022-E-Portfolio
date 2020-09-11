b77773d099a9dd2ca3389b05b4c9aa012a2b2adf
proguy18 <64591676+proguy18@users.noreply.github.com>
2020-09-11 17:34:33 +1000

    Added routes

--- e-portfolio/src/App.js
+++ e-portfolio/src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 
 
 // components
 import Header from './components/headerComponent/header';
 import Footer from './components/footerComponent/footer';
 import Homepage from './components/pages/homePage';
+import Login from './components/pages/login';
+import LoginForm from './components/pages/LoginForm'
 
 // includes
 import './Assets/css/default.min.css';
@@ -12,17 +19,22 @@ import './Assets/css/default.min.css';
 class App extends Component {
   render() {
     return (
+      <Router>
       <div className="App">
 
       <Header />
 
-        <Homepage />
+        <Route exact path='/' component={Homepage} />
+        <Route exact path='/Login' component={LoginForm} />
+
+
 
       <Footer />
 
 
 
       </div>
+      </Router>
     );
   }
 }

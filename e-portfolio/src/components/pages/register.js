578ba6a351a5c0c667071247127ae9ef5675ad69
proguy18 <64591676+proguy18@users.noreply.github.com>
2020-09-12 13:58:54 +1000

    Add files via upload

---
+++ e-portfolio/src/components/pages/register.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Register extends Component {
+  render() {
+    return (
+
+      <div className="container-fluid">
+        <p>
+        Please login or register to continue
+        </p>
+      </div>
+
+    );
+  }
+}
+
+export default Register;

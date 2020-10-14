9e85aea3b5c24b8f074eb36983202926feb86b1e
pewit <pweaus@gmail.com>
2020-10-14 23:39:43 +0800

    feat: add new Artist page

--- src/components/Artist/Artist.js
+++ src/components/Artist/Artist.js
@@ -1,40 +1,69 @@
-import React, { Component } from 'react';
-import animal from './animal.jpg';
-import food from './food.jpg';
-import people from './people.jpg';
-import travel from './travel.jpg';
-
-import './Artist.css';
-
-
-class Artist extends Component {
-  render() {
-    return (
-      <div>
-      <section>
-          <h1 className='intro'>Welcome to <b>Maddie's</b> portfolio.</h1>
-              <div class = "column">
-                  <div class = "row">
-                      <img className='animal' src={animal} alt='Animal'/>
-                      <img className='food' src={food} alt='Food'/>
-                  </div>
-                  <div class = "row">
-                      <img className='people' src={people} alt='People'/>
-                      <img className='travel' src={travel} alt='Travel'/>
-                  </div>
+import React, { Component } from "react";
+import animal from "./animal.jpg";
+import food from "./food.jpg";
+import people from "./people.jpg";
+import travel from "./travel.jpg";
+import avatar from "./avatar.svg";
+import "./Artist.scss";
+import Upload from '../ProfilePictureUpload';
+const galleyList = [animal,food,people,travel,animal,food,people]
+const Artist = (props) => {
+  return (
+    <section className="main-container">
+      <div className="user-info-box">
+        <div className="container">
+          <div className="row mb-3">
+            <div className="col-8 col-sm-12">
+              <div className="d-flex flex-column justify-content-center align-items-center">
+                <div className="avatar-box">
+                  <img src={avatar}></img>
+                </div>
+                <button className="btn btn-primary btn-sm mt-3 mb-3">
+                  Edit
+                </button>
+                <div className="item">Name: XXX</div>
+                <div className="item">Age: 18</div>
+                <div className="item">Gender: 18</div>
+                <div className="item">Location: xxx</div>
               </div>
-          <div className='text'>Name: Maddie<br/>Age: 24<br/>Major: Bachelor of Arts<br/>Address: -</div>
-        <br/>
-        <h3>About Me</h3>
-        <div className='aboutme'>
-          <h4>Paragraph 1 <br/><br/><h6>1st Paragraph</h6></h4>
-          <h4>Paragraph 2 <br/><br/><h6>2nd Paragraph</h6></h4>
-          <h4>Paragraph 3 <br/><br/><h6>3rd Paragraph</h6></h4>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-10 col-sm-12">
+              <div className="galley-box">
+                <h5>MY GALLEY</h5>
+                <div className="col-10 d-flex flex-wrap justify-content-center">
+                  {
+                    galleyList.map((item,index) => (
+                      <div className="galley-item mr-3 mb-1">
+                        <img src={item} key={index}></img>
+                      </div>
+                    ))
+                  }
+                  <Upload />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-10 col-sm-12">
+              <div className="box">
+                <h5>BLOG</h5>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-10 col-sm-12">
+              <div className="box">
+                <h5>FootPoint</h5>
+              </div>
+            </div>
+          </div>
         </div>
-      </section>
-    </div>
-    );
-  }
-}
+      </div>
+    </section>
+  );
+};
+
 
 export default Artist;

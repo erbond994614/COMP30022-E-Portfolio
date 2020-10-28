d37fc5f8442b7ba8030868ed93d31bf786610361
PWEIT <pweaus@gmail.com>
2020-10-28 22:27:23 +0800

    feat: update homepage

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -8,18 +8,18 @@ const Homepage = () => {
     <div className="container-fluid">
       <div className="row">
         <div className="col-xl-11 col-lg-11 col-md-11 col-sm-11 col-xs-11 mt-3">
-          <h1 className="text-center">Welcome to Quick e-Portfolio!</h1>
+          <h1 className="text-center h1">Welcome to Quick e-Portfolio!</h1>
         </div>
         <div className="col-xl-11 col-lg-11 col-md-11 col-sm-11 col-xs-11">
-          <p className="text-center">
+          <p className="text-center h2">
             Join today, create and manage your very own e-Portfolio website in 5
             minutes!
           </p>
-          <p className="text-center">
+          <p className="text-center h2">
             Using Quick e-Portfolio to make your own Blog, Foot Print, and
             upload your resume online!
           </p>
-          <p className="text-center">Sample of Personal e-Portfolio</p>
+          <p className="text-center h2">Sample of Personal e-Portfolio</p>
         </div>
       </div>
       <div className="row mt-3 justify-content-center">

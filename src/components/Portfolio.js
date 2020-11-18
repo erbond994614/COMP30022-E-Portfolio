3e79d6f8d265b6108e66b20fbf6d2e4471e0bbea
Pweit <pweaus@gmail.com>
2020-11-18 20:27:49 +0800

    feat: update portfolio page

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,36 +1,41 @@
 import React from "react";
-import { useSelector } from 'react-redux'
-import './Template/Template.css'
+import { useSelector } from "react-redux";
+import "./Template/Template.css";
 import { studentTemplate } from "./Template/templates";
-import ProfilePicture from './ProfilePicture'
-import Information from './Information'
-import AboutMe from './AboutMe'
-import Preview from './Preview'
+import ProfilePicture from "./ProfilePicture";
+import Information from "./Information";
+import AboutMe from "./AboutMe";
+import Preview from "./Preview";
 import Downloads from "./Downloads";
+import Avatar from "./Artist/Avatar";
 
 const Portfolio = () => {
-    var portfolio = useSelector(state => state.userAuth.user.portfolio)
-
+  var portfolio = useSelector((state) => state.userAuth.user.portfolio);
+  const user = useSelector((state) => state.userAuth.user);
   if (!portfolio) {
     portfolio = studentTemplate;
   }
 
-    return (
-        <div>
-            <section>
-                <div className='container'>
-                    {portfolio.info.Name ? <h1 className='intro'>Welcome to <b>{portfolio.info.Name}'s</b> portfolio.</h1> : <h1 className='intro'>Welcome to my portfolio</h1>}
-                          
-                    <ProfilePicture/>
+  return (
+    <div>
+      <section>
+        <div className="d-flex flex-column justify-content-center align-items-center">
+          {portfolio.info.Name ? (
+            <h1 className="intro">
+              Welcome to <b>{portfolio.info.Name}'s</b> portfolio.
+            </h1>
+          ) : (
+            <h1 className="intro">Welcome to my portfolio</h1>
+          )}
+          <Avatar avatar={user.avatar && user.avatar.data} />
+          <Information />
+        </div>
+        <br />
+        <AboutMe />
 
-                    <Information/>
-                </div>
-                <br/>
-                <AboutMe/>
-
-                <Downloads/>
-            </section>
-            <Preview/>
+        <Downloads />
+      </section>
+      <Preview />
     </div>
   );
 };
